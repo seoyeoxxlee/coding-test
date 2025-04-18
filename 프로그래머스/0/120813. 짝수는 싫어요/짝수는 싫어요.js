5a8846fdfb3388e23eec0c80852faecc59592689
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-04-18 23:26:01 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = [];
+    
+    for (let i=1; i<n+1; i++) {
+        if (i%2 === 0) {
+            continue;
+        } else {
+            answer.push(i);
+        }
+    }
+    return answer;
+}
