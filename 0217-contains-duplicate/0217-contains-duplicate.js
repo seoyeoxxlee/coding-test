1b56122e67112674875a9ac89f725bd4094f95e5
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-12 11:53:17 +0900

    Time: 18 ms (62.96%), Space: 72.5 MB (87.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+    const numSet = new Set();
+
+    for (let i=0; i<nums.length; i++) {
+        if (numSet.has(nums[i])) {
+            return true;
+        }
+        numSet.add(nums[i]);
+    }
+    return false;
+};
