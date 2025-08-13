e8c5a1cabd3ecc1433f7ce428019dc911995bf13
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-13 11:41:56 +0900

    Time: 9 ms (85.7%), Space: 56.6 MB (79.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,3 +1,13 @@
 var isAnagram = function(s, t) {
-    return s.split("").sort().join("") === t.split("").sort().join("") ? true : false;
+    if (s.length !== t.length) return false;
+
+    const count = {};
+    for (let i=0; i<s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) + 1;
+        count[t[i]] = (count[t[i]] || 0) - 1;
+    }
+    for (const key in count) {
+        if (count[key] !== 0) return false;
+    }
+    return true;
 };
