32fab52d726edf6f0f61e939e711100de80358b4
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-14 11:55:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 56.90 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,11 @@
 function solution(arr) {
-    return arr.filter((num, index) => num !== arr[index - 1]);
+    const stack = [];
+    
+    for (num of arr) {
+        // 스택의 peek와 다른 숫자만 스택에 push
+        if (stack[stack.length-1] !== num) {
+            stack.push(num);
+        }
+    }
+    return stack;
 }
