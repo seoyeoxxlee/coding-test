2b98aec6f2672485d87836c1879dafa2ba90cd36
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-18 16:41:51 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.58 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,7 +1,7 @@
 function solution(t, p) {
     let count = 0;
     for (let i=0; i<t.length-p.length+1; i++) {
-        if(t.substr(i, p.length) <= parseInt(p)) count ++;
+        if(t.substr(i, p.length) <= p) count ++;
     }
     return count;
 }
