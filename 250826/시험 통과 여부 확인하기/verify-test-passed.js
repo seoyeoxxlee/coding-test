0bdf5cc5a26d53bf5354410939b90752362f649d
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-26 16:45:21 +0900

    Title: 시험 통과 여부 확인하기, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250826/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let num = Number(fs.readFileSync(0).toString().trim());
+
+if (num >= 80) {
+    console.log("pass");
+}
+else {
+    console.log(`${80 - num} more score`);
+}
