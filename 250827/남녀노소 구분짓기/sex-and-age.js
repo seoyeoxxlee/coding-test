47ef8ec50b994f863fff2a667d2fbe7745d8799f
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-27 20:50:12 +0900

    Title: 남녀노소 구분짓기, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250827/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let gender = Number(input[0]);
+let age = Number(input[1]);
+
+if (gender === 0) {
+    if (age >= 19) {
+        console.log("MAN");
+    } else console.log("BOY");
+} else {
+    if (age >= 19) {
+        console.log("WOMAN");
+    } else console.log("GIRL");
+}
