4984d6a0693df0b5549a2f1d4368df7f551782f1
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-27 20:06:52 +0900

    Title: 굉장한 수, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250827/굉장한 수/amazing-number.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+if ((n % 2 === 1 && n % 3 === 0) || (n % 2 === 0 && n % 5 === 0)) {
+    console.log("true");
+} else console.log("false");
