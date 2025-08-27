86fc276fb4439b8ad18266458a98c505d01a7175
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-27 20:59:54 +0900

    Title: 윤년인가, Time: 170ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250827/윤년인가/is-leap-year.js
+++ 250827/윤년인가/is-leap-year.js
@@ -4,11 +4,15 @@ const input = fs.readFileSync(0).toString().trim();
 let year = Number(input);
 
 if (year % 4 === 0) {
-    if (year % 100 !== 0) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            console.log("true");
+        } else {
+            console.log("false");
+        }
+    } else {
         console.log("true");
-    } else console.log("false");
+    }
 } else {
-    if (year % 400 === 0) {
-        console.log("true");
-    } else console.log("false");
+    console.log("true");
 }
