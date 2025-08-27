ab05d2df5d5817e80580baaf24b72ed223baa86a
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-27 20:04:35 +0900

    Title: 정수의 조건 여부 3, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250827/정수의 조건 여부 3/numbers-condition-3.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+let a = Number(input);
+
+if (a % 13 === 0 || a % 19 === 0) {
+    console.log("True");
+} else {
+    console.log("False");
+}
