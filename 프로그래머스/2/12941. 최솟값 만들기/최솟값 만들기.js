bfdc3f49d0bd73952dc52c78d47557d4e88db7df
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-29 23:57:55 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.75 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,9 +2,6 @@ function solution(A,B){
     A = A.sort((a, b) => a - b);
     B = B.sort((a, b) => b - a);
     
-    let answer = 0;
-    for (let i=0; i<A.length; i++) {
-        answer += A[i] * B[i];
-    }
-    return answer;
+    return A.reduce((total, cur, i) => total + cur * B[i], 0);
 }
+
