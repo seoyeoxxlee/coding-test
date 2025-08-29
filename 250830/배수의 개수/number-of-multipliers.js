064efba3cbf9120acd1ff3a5dfc84414cf5ed3a0
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-08-30 02:11:51 +0900

    Title: 배수의 개수, Time: 163ms, Memory: 15MB, Status: Passed - Codetree

--- 250830/배수의 개수/number-of-multipliers.js
+++ 250830/배수의 개수/number-of-multipliers.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
-const numbers = input.split(" ").map(Number);
-
-console.log(numbers);
+const numbers = input.split("\n").map(Number);
 
 let cnt1 = 0;
 let cnt2 = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 3 === 0) {
+    if ((numbers[i] % 3 === 0) && (numbers[i] % 5 === 0)) {
         cnt1++;
+        cnt2++;
     } else if (numbers[i] % 5 === 0) {
         cnt2++;
+    } else if (numbers[i] % 3 === 0) {
+        cnt1++;
     }
 }
 
 console.log(cnt1 + " " + cnt2);
-
