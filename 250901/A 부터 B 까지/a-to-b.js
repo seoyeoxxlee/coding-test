4e4312552dd997bff8a108817c12d8432dd3070d
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-09-01 16:47:34 +0900

    Title: A 부터 B 까지, Time: 194ms, Memory: 15MB, Status: Passed - Codetree

--- 250901/A 부터 B 까지/a-to-b.js
+++ 250901/A 부터 B 까지/a-to-b.js
@@ -4,17 +4,15 @@ const input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-let res = [];
-res.push(a);
+let res = "";
 
-while (a < b) {
+while(a <= b) {
+    res += a + " ";
     if (a % 2 === 1) {
-        res.push(a * 2)
-        a = a * 2;
+        a *= 2;
     } else {
-        res.push(a + 3);
-        a = a + 3;
+        a += 3;
     }
 }
 
-console.log(res.join(" "));
+console.log(res);
