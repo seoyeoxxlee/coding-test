4960cf9ee318aa25a7c3c901e624104cc320c75e
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-09-03 16:00:40 +0900

    Title: 직각 삼각형 출력, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250903/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+for (let i=1; i<=n; i++) {
+    str = "";
+    for (let j=0; j<i; j++) {
+        str += "*" + " ";
+    }
+    console.log(str);
+}
