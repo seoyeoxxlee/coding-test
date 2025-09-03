e8eaa7e56458ae31a8425798d59617cd62fd6922
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-09-03 17:24:11 +0900

    Title: 짝수의 합 n번 구하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0]);
+
+for (let i = 1; i <= n; i++) {
+    let x = input[i].split(" ");
+    let a = Number(x[0]);
+    let b = Number(x[1]);
+    let ans = 0;
+
+    for (let j = a; j <= b; j++) {
+        if (j % 2 === 0) {
+            ans += j;
+        }
+    }
+    console.log(ans);
+}
