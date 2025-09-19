faaa4a9d8217ca10587fc64eebd7f041279ec0ba
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-09-20 00:33:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+function solution(price) {
+  switch (true) {
+    case price >= 500000:
+      return Math.floor(price * 0.8);
+    case price >= 300000:
+      return Math.floor(price * 0.9);
+    case price >= 100000:
+      return Math.floor(price * 0.95);
+    default:
+      return price;
+  }
+}
