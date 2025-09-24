a6c109d2c5b56d31298981c565f84fcbfc103ac1
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-09-24 16:07:12 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    for (let i=1; i<=1000; i++) {
-        if (n / i === i && n % i === 0) {
-            return 1;
-        }
-    }
-    return 2;
+    return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
