9ffdb00918dd774f0253117053bf547de84be0f2
seoyeong <seoyeonglee@umail.ucsb.edu>
2025-10-05 23:58:23 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let count = 0;
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            count++;
+            if (i !== n / i) count++; 
+        }
+    }
+    return count;
+}
